6e376d24e1e9e917c4a1508eb0dcabbbc3ae9f83
Moka- <ebmoha@gmail.com>
2018-02-12 22:38:45 +0200

    app launches now

---
+++ src/actions/index.js
@@ -0,0 +1,15 @@
+export const addTodo = (text) => ({
+  type: 'ADD_TODO',
+  id: 1,
+  text
+})
+
+export const setVisibilityFilter = (filter) => ({
+  type: 'SET_VISIBILITY_FILTER',
+  filter
+})
+
+export const toggleTodo = (id) => ({
+  type: 'TOGGLE_TODO',
+  id
+})
--- src/containers/root/Root.js
+++ src/containers/root/Root.js
@@ -1,5 +1,27 @@
-if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./Root.prod')
-} else {
-  module.exports = require('./Root.dev')
+import React from 'react'
+import Routes from './Routes'
+import Header from '../../components/header/header'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import Reboot from 'material-ui/Reboot'
+import './Root.css'
+import theme from './Theme'
+import DevTools from './DevTools'
+import { Provider } from 'react-redux'
+import PropTypes from 'prop-types'
+
+const Root = ({ store }) => (
+  <Provider store={store}>
+    <MuiThemeProvider theme={theme}>
+        <Header />
+        <Routes />
+        <DevTools />
+        <Reboot />
+    </MuiThemeProvider>
+  </Provider>
+)
+
+Root.propTypes = {
+  store: PropTypes.object.isRequired,
 }
+
+export default Root
